43b3d30dcfd1c3629177ed11bd5da4d570f5b4ec
Iván <ivan.leris@enti.cat>
2024-11-12 21:24:23 +0100

    Secuencia buscar target y dejarlo en el workBench

--- Assets/Scripts/MyRobotController.cs
+++ Assets/Scripts/MyRobotController.cs
@@ -1,6 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+enum RobotState
+{
+    P_target,
+    P_WorkBench,
+    P_InitialPosition
+};
 
 public class MyRobotController : MonoBehaviour
 {
@@ -9,7 +17,14 @@ public class MyRobotController : MonoBehaviour
     public Transform Joint2;
     public Transform endFactor;
     public Transform Stud_target;
+    public Transform Workbench_destination;
 
+    public Transform InitialPosition;
+
+    public bool pickTarget = false;
+
+    private bool hasPickedUp = false;
+    private RobotState robotState = RobotState.P_target;
 
     public float alpha = 0.1f;
     private float tolerance = 1f;
@@ -22,6 +37,9 @@ public class MyRobotController : MonoBehaviour
     private float l2;
     private float l3;
 
+    private float maxDistance;
+    private float distance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,39 +47,123 @@ public class MyRobotController : MonoBehaviour
         l2 = Vector3.Distance(Joint1.position, Joint2.position);
         l3 = Vector3.Distance(Joint2.position, endFactor.position);
 
+        InitialPosition.position = endFactor.position;
+
+        maxDistance = l1 + l2 + l3;
+
         costFunction = Vector3.Distance(endFactor.position, Stud_target.position) * Vector3.Distance(endFactor.position, Stud_target.position);
+
         theta = Vector3.zero;
+
+        distance = Vector3.Distance(Joint0.position, Stud_target.position);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //distance = Vector3.Distance(Joint0.position, Stud_target.position);
 
+        //if (distance <= maxDistance)
+        //{
+            
+        if(pickTarget)
+        {
+            PickStudAnim();
+        }
+            
+        //}
+
+        
+    }
+
+    void PickStudAnim()
+    {
+        switch(robotState)
+        {
+            case RobotState.P_target:
+
+                MoveRobot(Stud_target);
+
+                break;
+
+            case RobotState.P_WorkBench:
+
+                MoveTarget(Stud_target);
+                MoveRobot(Workbench_destination);
+
+                break;
+
+            case RobotState.P_InitialPosition:
+
+                MoveRobot(InitialPosition);
+
+                break;
+
+            default: 
+                break; 
+        }
+
+    }
+
+    void MoveRobot(Transform target)
+    {
+        costFunction = Vector3.Distance(endFactor.position, target.position) * Vector3.Distance(endFactor.position, target.position);
+
+        //Comprobamos que tocamos el target
         if (costFunction > tolerance)
         {
 
-            gradient = CalculateGradient();
+            gradient = CalculateGradient(target);
             theta += -alpha * gradient;
             endFactor.position = GetEndFactorPosition();
 
 
             Joint1.position = GetJoint1Position();
             Joint2.position = GetJoint2Position();
+
+        }
+        else
+        {
+            switch(robotState)
+            { 
+                case RobotState.P_target:
+
+                    robotState = RobotState.P_WorkBench;
+
+                    break;
+
+                case RobotState.P_WorkBench:
+
+                    robotState = RobotState.P_InitialPosition;
+
+                    break;
+
+                case RobotState.P_InitialPosition:
+                    
+                    
+
+                    break;
+
+            default:
+            break;
+            }
         }
 
-        costFunction = Vector3.Distance(endFactor.position, Stud_target.position) * Vector3.Distance(endFactor.position, Stud_target.position);
 
-
-
+        
     }
 
+    void MoveTarget(Transform target)
+    {
+        target.position = endFactor.position;
+    }
 
-    Vector3 CalculateGradient()
+    Vector3 CalculateGradient(Transform target)
     {
 
         Vector3 gradientVector;
 
-        Vector3 coeff = 2 * (endFactor.position - Stud_target.position);
+        Vector3 coeff = 2 * (endFactor.position - target.position);
 
         gradientVector.x = -coeff.x * (l1 * Mathf.Sin(theta.x)
             + l2 * Mathf.Sin(theta.x + theta.y)
